11e4354c7d77002a0462822dbe5b669d2a27978e
mhebd <mhemonhossain@gmail.com>
2023-12-09 01:24:18 +0600

    Backend setup done

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,11 @@
-// Show modal
+// Show modal and upload attachment
 (function() {
   const attachmentBtn = document.querySelectorAll('.attachment-btn');
   const modal = document.querySelector('.modal');
-
+  let targetCard;
   attachmentBtn.forEach(btn => {
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', (e) => {
+      targetCard = e.target.parentElement.parentElement.querySelector('.attachment-btn').querySelector('span');
       modal.classList.add('show');
     });
   })
@@ -14,6 +15,46 @@
       modal.classList.remove('show');
     }
   })
+
+  const form = document.getElementById("uploadForm");
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const attachments = form.attachments.files;
+    const formData = new FormData();
+    // Append each selected file to the FormData object
+    for (let i = 0; i < attachments.length; i++) {
+      formData.append('attachments', attachments[i]);
+    }
+
+    // Fetch API
+    fetch('/api/v1/attachment-upload', {
+        method: 'POST',
+        body: formData,
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.status === 'success') {
+          targetCard.innerText = targetCard.innerText * 1 + data.filenames.length;
+          const fileList = document.getElementById("fileList");
+          data.filenames.forEach(function(filename) {
+            const listItem = document.createElement("li");
+            listItem.textContent = filename;
+            fileList.appendChild(listItem);
+          });
+        } else {
+          alert("File upload failed: " + data.message);
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  })
+
 })();
+
+
+
+
 
 console.log('Hello Developer!!');
