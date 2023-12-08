b52f685f5b43dd674c5be44bd9d4e1f315ebdabd
mhebd <mhemonhossain@gmail.com>
2023-12-08 19:19:36 +0600

    Fron-end design done

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,19 @@
+// Show modal
+(function() {
+  const attachmentBtn = document.querySelectorAll('.attachment-btn');
+  const modal = document.querySelector('.modal');
+
+  attachmentBtn.forEach(btn => {
+    btn.addEventListener('click', () => {
+      modal.classList.add('show');
+    });
+  })
+
+  modal.addEventListener('click', e => {
+    if (e.target.classList.contains('modal')) {
+      modal.classList.remove('show');
+    }
+  })
+})();
+
 console.log('Hello Developer!!');
